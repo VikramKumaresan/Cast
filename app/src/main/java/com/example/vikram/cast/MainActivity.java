87a6e4e4e8ad1e7f248b57a2257e4f8db039de0a
Vikram <vikramkumaresan@rocketmail.com>
2018-12-21 14:55:49 +0530

    Handle permission rejection

--- app/src/main/java/com/example/vikram/cast/MainActivity.java
+++ app/src/main/java/com/example/vikram/cast/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.vikram.cast;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.vikram.cast.clientCode.ClientActivity;
 import com.example.vikram.cast.serverCode.ServerActivity;
 
+import java.security.Permission;
+
 public class MainActivity extends AppCompatActivity {
+    private PermissionManager permissionManagerObj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +30,13 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this,ServerActivity.class);
         startActivity(myIntent);
     }
-
     public void goToDiscoverActivity(View view){
         Intent myIntent = new Intent(this,ClientActivity.class);
         startActivity(myIntent);
     }
 
     private void checkPermissions(){
-        PermissionManager permissionManagerObj = new PermissionManager(this);
+        permissionManagerObj = new PermissionManager(this);
 
         if(!permissionManagerObj.checkPermission(Manifest.permission.ACCESS_WIFI_STATE) ||
                 !permissionManagerObj.checkPermission(Manifest.permission.CHANGE_WIFI_STATE) ||
@@ -38,13 +44,20 @@ public class MainActivity extends AppCompatActivity {
                 !permissionManagerObj.checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)||
                 !permissionManagerObj.checkPermission(Manifest.permission.BLUETOOTH_ADMIN)){
 
-            permissionManagerObj.askPermission(new String[] {
-                    Manifest.permission.ACCESS_WIFI_STATE,
-                    Manifest.permission.CHANGE_WIFI_STATE,
-                    Manifest.permission.INTERNET,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.BLUETOOTH_ADMIN
-            });
+            permissionManagerObj.askPermission(permissionManagerObj.getPermissionList());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==PermissionManager.REQUEST_CODE){
+            for (int permission : grantResults){
+                if(permission==PackageManager.PERMISSION_DENIED){
+                    Toast.makeText(this, "Pls Grant Permissions", Toast.LENGTH_LONG).show();
+                    finish();
+                    return;
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/vikram/cast/PermissionManager.java
+++ app/src/main/java/com/example/vikram/cast/PermissionManager.java
@@ -1,16 +1,25 @@
 package com.example.vikram.cast;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
 public class PermissionManager {
-
+    final static int REQUEST_CODE = 1;
     private Activity requestingActivity;
+    private String[] permissionList;
 
     PermissionManager(Activity activity){
         requestingActivity=activity;
+        permissionList= new String[]{
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.BLUETOOTH_ADMIN
+        };
     }
 
     boolean checkPermission(String permission){
@@ -23,7 +32,11 @@ public class PermissionManager {
     }
 
     void askPermission(String[] permissionList){
-        ActivityCompat.requestPermissions(requestingActivity,permissionList,1);
+        ActivityCompat.requestPermissions(requestingActivity,permissionList,REQUEST_CODE);
+    }
+
+    String[] getPermissionList() {
+        return permissionList;
     }
 }
 
